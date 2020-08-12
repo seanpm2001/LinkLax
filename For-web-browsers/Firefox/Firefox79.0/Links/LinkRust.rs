0d904e938f2ab113a6eade72c62f2078587bf58c
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-12 13:53:01 -0700

    LinkRust.rs
    A basic Rust script for link coloring.

---
+++ For-web-browsers/Firefox/Firefox79.0/Links/LinkRust.rs
@@ -0,0 +1,21 @@
+// Start of script
+// Disclaimer:
+// I have never written in Rust before, other than copying samples from Wikipedia.
+// I am trying to make this work by referencing a random file from the rust-lang repository.
+// I am not sure if this script will work. There aren't very many visible sources for this language for me.
+fn main() {
+  println!("Rust for Mozilla Firefox 79.0");
+}
+fn color() {
+  println("Colors for hyperlinks");
+  println("Security")
+  println("http - color: red (change) #RRRRRR");
+  println("https - color: green (change) - #GGGGGG");
+  println("Path");
+  println("File:// - color: orange (change) - #FF7F00");
+}
+// File info
+// File version: 1 (August 12th 2020 at 1:53 pm)
+// File type: *.rs (Rust script file)
+// Line count (including blank lines and compiler line):  22
+// End of script
