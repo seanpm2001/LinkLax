912913a10d6aec5c4e88bb2363a10b67171ce831
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-28 16:05:33 -0700

    MAIN_V1.rs
    An archive of the first version of the Main script for this directory.

---
+++ For-the-terminal/Konsole/OldVersions/Main/1/1-100/MAIN_V1.rs
@@ -0,0 +1,76 @@
+// Start of script
+// Disclaimer:
+// I have very limited experience with Rust, so this script may not be functional, and may come off as bad programming.
+use std::io;
+union KonsoleLax() {
+// Definitions
+proClass = str("Konsole"); // Platform name
+proUUID = str("LinkLax:Konsole:x64"); // Platform ID
+let maxAttempts = 10; // Maximum number of attempts before not being enabled
+laxOF = bool("True"); // Defines the extension being on or off
+fn main() { // Main method
+	println!("Linklax for the terminal: Konsole (KDE)");
+	return debug1();
+	return konsole();
+	mut return man();
+	// return colorhttp();
+	break;
+}
+fn man() { // Manual entry
+	"""
+	LinkLax for Konsole
+	
+	About:
+	LinkLax is a program extension for text editors and web browsers that adds full customization to hyperlinks, allowing you to change them to different colors or have them be animated. This can also be an important safety feature, as it can help you recognize and get out of dangerous sites faster. 
+	 
+	Specs:
+	
+	This extension is written in:
+	1. Rust 100%
+	
+	Platforms:
+	Kubuntu, Ubuntu, Lubuntu, Xubuntu, Edubuntu, Fedora, CentOS, Linux Mint, Linux/other (KDE is not required to install Konsole)
+	
+	Commands:
+	
+	To enable/disable LinkLax:
+	
+	laxOF -toggle (stands for LinkLax On/Off)
+	
+	Debugging:
+	
+	LinkLax will try to load in 10 attempts. If the program does not load in 10 attempts, it will not be active for the current Konsole session.
+	You can change the number of attempts by modifying line 8 of /LinkLax/For-the-terminal/Konsole/MAIN.rs and changing maxAttempts to any number you desire (10 is recommended, although you might need to change it in rare circumstances)
+	
+	End of manual entry, manual is incomplete
+	"""
+}
+fn konsole() { // Special Konsole data
+	println!("Konsole rust");
+	println!("Options for the KDE terminal");
+	println!("");
+}
+fn colorhttp() { // For web hyperlinks only
+	println!("Colors for hyperlinks");
+	println!("Security")
+	println!("http - color: red (change) #RRRRRR");
+	println!("https - color: green (change) - #GGGGGG");
+	println!("Path");
+	println!("File:// - color: orange (change) - #FF7F00");
+	break;
+}
+fn debug1() { // Debugging data A
+	io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+        io:::stdin()
+        // Blank method
+        );
+}
+// Divider
+} // End of union
+// File info
+// File version: 1 (Wednesday, April 28th 2021 at 4:05 pm)
+// File type: Rust source file (*.rs)
+// Line count (including blank lines and compiler line): 77
+// End of script
